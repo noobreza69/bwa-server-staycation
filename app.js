a13c6ef9cf955e50e92af57802192bbcb8173bfa
Reza <rezaaulia51@gmail.com>
2023-09-12 14:28:48 +0700

    deploy

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
-
+const cors =require('cors')
 
 //monggoseee
 const mongoose = require('mongoose');
@@ -44,7 +44,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/sb-admin-2', express.static(path.join(__dirname, 'node_modules/startbootstrap-sb-admin-2')));
-
+app.use(cors())
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 // admin
@@ -55,6 +55,7 @@ app.use('/api/v1/member', apiRouter);
 app.use(function (req, res, next) {
   next(createError(404));
 });
+
 
 
 // error handler
