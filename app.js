85cf0290eae109537361931602e04b1f60e51d3a
Reza <rezaaulia51@gmail.com>
2023-09-12 14:20:28 +0700

    deploy

--- app.js
+++ app.js
@@ -56,10 +56,7 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 
-const cors = require('cors');
-app.use(cors({
-    origin: ['http://localhost:3000/']
-}));
+
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
