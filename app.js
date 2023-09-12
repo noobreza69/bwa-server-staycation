4e8bb7bfcad48a93afd2e9125a7f3322893bcbee
Reza <rezaaulia51@gmail.com>
2023-09-12 14:15:00 +0700

    deploy

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
-const cors= require('cors');
+
 
 //monggoseee
 const mongoose = require('mongoose');
